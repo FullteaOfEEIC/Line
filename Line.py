46e86aeab660fcf3e207d2fc7cdb9bf29e5f3fe4
Rikuta <rikuta0811@g.ecc.u-tokyo.ac.jp>
2019-03-15 19:24:21 +0900

    add: some

--- Line.py
+++ Line.py
@@ -2,18 +2,41 @@ import requests
 import json
 from keys import CHANNEL_ACCESS_TOKEN, me
 
-def sendLINE(content,to=me,CHANNEL_ACCESS_TOKEN=CHANNEL_ACCESS_TOKEN):
-    #LINEアカウントにアクセスするためのトークン
 
+def sendLINE(content, type="text", to=me, CHANNEL_ACCESS_TOKEN=CHANNEL_ACCESS_TOKEN):
+    # LINEアカウントにアクセスするためのトークン
 
     url = "https://api.line.me/v2/bot/message/push"
     headers = {
-    "Content-Type" : "application/json; charset=UTF-8",
-    'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN
+        "Content-Type": "application/json; charset=UTF-8",
+        'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN
     }
-    data={"to":to,"messages":[{"type":"text","text":content}]}
+    data = createData(type, to)
 
-    requests.post(url,data=json.dumps(data),headers=headers)
+    print(requests.post(url, data=json.dumps(data), headers=headers).text)
 
-if __name__=="__main__":
-    sendLINE("テスト送信です")
+
+def createData(type, to, *, text=None, stickerId=None, packageId=None, originalContentUrl=None, previewImageUrl=None):
+    data = {"to": to}
+    if type == "text":
+        assert text != None
+        messages = {"type": "text", "text": content}
+    elif type == "sticker":
+        assert stickerId != None
+        assert packageId != None
+        messages = {"type": "sticker",
+                    "packageId": packageId, "stickerId": stickerId}
+    elif type == "image":
+        messages = {"type": "image", "originalContentUrl": originalContentUrl,
+                    "previewImageUrl": previewImageUrl}
+    elif type == "video":
+        messages = {"type": "video", "originalContentUrl": originalContentUrl,
+                    "previewImageUrl": previewImageUrl}
+
+
+    data["messages"] = [messages]
+    return data
+
+
+if __name__ == "__main__":
+    sendLINE("テスト送信です", type="image")
